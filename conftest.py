677411302e4f715d798233d237601efcc2e9e8ee
shoukatshah <shoukatanjum567@gmail.com>
2025-12-24 11:56:15 +0500

    Modified the conftest file

--- conftest.py
+++ conftest.py
@@ -1,10 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
-from utils.browser import get_driver
 import time
 
 @pytest.fixture(scope="class")
 def driver():
-    driver = get_driver()
+    options = Options()
+
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False
+    }
+
+    options.add_experimental_option("prefs", prefs)
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-save-password-bubble")
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
     yield driver
     time.sleep(10)
     #input("Press enter key to quit.....")
